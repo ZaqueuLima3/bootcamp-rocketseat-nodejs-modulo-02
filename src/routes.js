a16a27da5597aab6ca0f398c728b174b62715eea
zaqueu lima dos santos <zaqueulima1@gmail.com>
2019-07-11 08:43:54 -0300

    Creates user controller and password hash

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Zaqueu Lima',
-    email: 'zaqueulima1@gmail.com',
-    password_hash: '123456789',
-  });
-
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
